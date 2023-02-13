91e9eeb19ac22df77a83224c71c23812087b4651
yutojubako <ytim8812@keio.jp>
2023-02-14 01:55:19 +0900

    [Add] StratifiedKFold

--- main.py
+++ main.py
@@ -156,6 +156,10 @@ if __name__ == "__main__":
         transforms.ToPILImage(),
         transforms.Resize(224),
         transforms.Grayscale(num_output_channels=3),
+        # transforms.RandomOrder([
+        #     transforms.RandomResizedCrop(224, scale=(0.9, 1.1), ratio=(0.9, 1.1)),
+        #     transforms.RandomAffine(degrees=10)
+        # ]),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
@@ -163,73 +167,89 @@ if __name__ == "__main__":
     train_dataset = KMNISTDataset(train_df[ID], train_df[TARGET], PATH['train_image_dir'], transform=transform)
     valid_dataset = KMNISTDataset(valid_df[ID], valid_df[TARGET], PATH['train_image_dir'], transform=transform)
 
-    # DataLoaderを用いてバッチサイズ分のデータを生成します。shuffleをtrueにすることでデータをshuffleしてくれます
     train_dataloader = DataLoader(train_dataset, batch_size=PARAMS['batch_size'], shuffle=True)
     valid_dataloader = DataLoader(valid_dataset, batch_size=PARAMS['valid_batch_size'], shuffle=False)
     
-    model = ResNet().to(DEVICE)
-    
-    optim = Adam(model.parameters(), lr=PARAMS['lr'])
-    criterion = nn.CrossEntropyLoss()
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, mode='min', factor=0.8, patience=1)
 
 
-    for epoch in range(PARAMS['epochs']):
-        # epochループを回す
-        model.train()
-        train_loss_list = []
-        train_accuracy_list = []
-        
-        for x, y in tqdm(train_dataloader):
-            # 先ほど定義したdataloaderから画像とラベルのセットのdataを取得
-            x = x.to(dtype=torch.float32, device=DEVICE)
-            y = y.to(dtype=torch.long, device=DEVICE)
+    k = 5
+    skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=SEED)
+
+    for fold, (train_idx, valid_idx) in enumerate(skf.split(train_df[ID], train_df[TARGET])):
+        # Setting of model
+        model = ResNet().to(DEVICE)
+        optim = Adam(model.parameters(), lr=PARAMS['lr'])
+        criterion = nn.CrossEntropyLoss()
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, mode='min', factor=0.8, patience=1)
+
+        # Setting of dataset
+        tr_df = train_df.iloc[train_idx].reset_index(drop=True)
+        val_df = train_df.iloc[valid_idx].reset_index(drop=True)
+
+        train_dataset = KMNISTDataset(tr_df[ID], tr_df[TARGET], PATH['train_image_dir'], transform=transform)
+        valid_dataset = KMNISTDataset(val_df[ID], val_df[TARGET], PATH['train_image_dir'], transform=transform)
+
+        train_dataloader = DataLoader(train_dataset, batch_size=PARAMS['batch_size'], shuffle=True)
+        valid_dataloader = DataLoader(valid_dataset, batch_size=PARAMS['valid_batch_size'], shuffle=False)
+
+
+        # Training
+        for epoch in range(PARAMS['epochs']):
+            # epochループを回す
+            model.train()
+            train_loss_list = []
+            train_accuracy_list = []
             
-            # pytorchでは通常誤差逆伝播を行う前に毎回勾配をゼロにする必要がある
-            optim.zero_grad()
-            # 順伝播を行う
-            y_pred = model(x)
-            # lossの定義 今回はcross entropyを用います
-            loss = criterion(y_pred, y)
-            # 誤差逆伝播を行なってモデルを修正します(誤差逆伝播についてはhttp://hokuts.com/2016/05/29/bp1/)
-            loss.backward() # 逆伝播の計算
-            # 逆伝播の結果からモデルを更新
-            optim.step()
-            
-            train_loss_list.append(loss.item())
-            train_accuracy_list.append(accuracy_score_torch(y_pred, y))
-            wandb.log({'train_loss': loss.item(), 'train_accuracy': accuracy_score_torch(y_pred, y)})
-
-        model.eval()
-        valid_loss_list = []
-        valid_accuracy_list = []
-
-        for x, y in tqdm(valid_dataloader):
-            x = x.to(dtype=torch.float32, device=DEVICE)
-            y = y.to(dtype=torch.long, device=DEVICE)
-            
-            with torch.no_grad():
+            for x, y in tqdm(train_dataloader):
+                # 先ほど定義したdataloaderから画像とラベルのセットのdataを取得
+                x = x.to(dtype=torch.float32, device=DEVICE)
+                y = y.to(dtype=torch.long, device=DEVICE)
+                
+                # pytorchでは通常誤差逆伝播を行う前に毎回勾配をゼロにする必要がある
+                optim.zero_grad()
+                # 順伝播を行う
                 y_pred = model(x)
+                # lossの定義 今回はcross entropyを用います
                 loss = criterion(y_pred, y)
+                # 誤差逆伝播を行なってモデルを修正します(誤差逆伝播についてはhttp://hokuts.com/2016/05/29/bp1/)
+                loss.backward() # 逆伝播の計算
+                # 逆伝播の結果からモデルを更新
+                optim.step()
+                
+                train_loss_list.append(loss.item())
+                train_accuracy_list.append(accuracy_score_torch(y_pred, y))
+                wandb.log({'train_loss': loss.item(), 'train_accuracy': accuracy_score_torch(y_pred, y)})
+
+            model.eval()
+            valid_loss_list = []
+            valid_accuracy_list = []
+
+            for x, y in tqdm(valid_dataloader):
+                x = x.to(dtype=torch.float32, device=DEVICE)
+                y = y.to(dtype=torch.long, device=DEVICE)
+                
+                with torch.no_grad():
+                    y_pred = model(x)
+                    loss = criterion(y_pred, y)
+                
+                valid_loss_list.append(loss.item())
+                valid_accuracy_list.append(accuracy_score_torch(y_pred, y))
+                wandb.log({'valid_loss': loss.item(), 'valid_accuracy': accuracy_score_torch(y_pred, y)})
             
-            valid_loss_list.append(loss.item())
-            valid_accuracy_list.append(accuracy_score_torch(y_pred, y))
-            wandb.log({'valid_loss': loss.item(), 'valid_accuracy': accuracy_score_torch(y_pred, y)})
+            print('epoch: {}/{} - loss: {:.5f} - accuracy: {:.3f} - val_loss: {:.5f} - val_accuracy: {:.3f}'.format(
+                epoch,
+                PARAMS['epochs'], 
+                np.mean(train_loss_list),
+                np.mean(train_accuracy_list),
+                np.mean(valid_loss_list),
+                np.mean(valid_accuracy_list)
+            ))
         
-        print('epoch: {}/{} - loss: {:.5f} - accuracy: {:.3f} - val_loss: {:.5f} - val_accuracy: {:.3f}'.format(
-            epoch,
-            PARAMS['epochs'], 
-            np.mean(train_loss_list),
-            np.mean(train_accuracy_list),
-            np.mean(valid_loss_list),
-            np.mean(valid_accuracy_list)
-        ))
-    
-        if best_score <= np.mean(valid_accuracy_list):
-            best_score = np.mean(valid_accuracy_list)
-            best_sd = model.state_dict()
-        scheduler.step(np.mean(valid_loss_list))
-    state_dicts.append(best_sd)
+            if best_score <= np.mean(valid_accuracy_list):
+                best_score = np.mean(valid_accuracy_list)
+                best_sd = model.state_dict()
+            scheduler.step(np.mean(valid_loss_list))
+        state_dicts.append(best_sd)
 
     test_dataset = KMNISTDataset(
         sample_submission_df[ID],
